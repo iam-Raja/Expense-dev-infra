09f4df199e5295aecd20bf079ae493e4bcf62cd7
iam-raja <rajapeta45@gmail.com>
2024-06-22 11:24:29 +0530

    demo

--- 08.PUBLIC-ALB/data.tf
+++ 08.PUBLIC-ALB/data.tf
@@ -6,6 +6,8 @@ data "aws_ssm_parameter" "public_alb_sg_id" {
   name = "/${var.project_name}/${var.environment}/SG_ID_PUBLIC-ALB"
 }
 
-data "aws_ssm_parameter" "pub_alb_listner_arn_https" {
-  name = "/${var.project_name}/${var.environment}/pub_alb_listner_arn_https"
+data "aws_ssm_parameter" "acm_certificate_arn" {
+  name = "/${var.project_name}/${var.environment}/acm_certificate_arn"
 }
+
+
